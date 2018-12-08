5fec53d3acb13639abd3e16ecacbf106a4ed6627
Jeffrey Almonte <jalmonte83@gmail.com>
2018-12-08 16:41:04 -0500

    storyboard

---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Contact.swift
@@ -0,0 +1,9 @@
+//
+//  Contact.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Jeffrey Almonte on 12/7/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import Foundation
---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Stock.swift
@@ -0,0 +1,14 @@
+//
+//  Stock.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Jeffrey Almonte on 12/7/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+
+struct Stock: Codable {
+    
+}
