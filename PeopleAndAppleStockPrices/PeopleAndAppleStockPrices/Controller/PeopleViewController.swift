23c15e22d8a51b2e3091fb2b24e9634f82c8352a
John Doe <leandro.wauters@gmail.com>
2018-12-07 19:34:50 -0500

    Shows names and city

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controller/PeopleViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controller/PeopleViewController.swift
@@ -10,24 +10,49 @@ import UIKit
 
 class PeopleViewController: UIViewController {
     
+    var people = [People]()
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    loadData()
-    
+    myTableView.dataSource = self
+    searchPeople()
   }
 
-    func loadData(){
+    private func searchResult(completion: @escaping([People]?) -> Void){
         if let path = Bundle.main.path(forResource: "userinfo", ofType: "json"){
             let myUrl = URL.init(fileURLWithPath: path)
             if let data = try? Data.init(contentsOf: myUrl){
                 do {
                     let searchData = try JSONDecoder().decode(People.searchData.self, from: data)
-                    dump(searchData)
+                    completion(searchData.results)
                 } catch {
                     print(error)
                 }
             }
         }
     }
+    private func searchPeople(){
+        searchResult{people in
+            if let peopleResult = people{
+                self.people = peopleResult
+            }
+        }
+    }
 }
-
+extension PeopleViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath)
+        let user = people[indexPath.row]
+        cell.textLabel?.text = "\(user.name.first.capitalized) \(user.name.last.capitalized)"
+        cell.detailTextLabel?.text = user.location.city.capitalized
+        return cell
+    }
+}
