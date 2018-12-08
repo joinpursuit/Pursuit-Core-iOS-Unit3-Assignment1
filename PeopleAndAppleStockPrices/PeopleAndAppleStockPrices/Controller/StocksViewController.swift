c06491514acfc6b95eca7695eca2cf76e37273aa
John Doe <leandro.wauters@gmail.com>
2018-12-08 17:23:11 -0500

    Dump Data Worked

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controller/StocksViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controller/StocksViewController.swift
@@ -9,22 +9,45 @@
 import UIKit
 
 class StocksViewController: UIViewController {
-
+    var stocks = [Stocks]()
+   
+    @IBOutlet weak var stocksTableView: UITableView!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        stocksTableView.dataSource = self
+        loadData()
+        dump(stocks)
+    }
+    func loadData(){
+        if let path = Bundle.main.path(forResource: "applstockinfo", ofType: "json"){
+            let myUrl = URL.init(fileURLWithPath: path)
+            if let data = try? Data.init(contentsOf: myUrl){
+                do {
+                    self.stocks = try JSONDecoder().decode([Stocks].self, from: data)
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
 
-        // Do any additional setup after loading the view.
+
+}
+extension StocksViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
-    */
-
+    
+    
 }
---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Model/Stocks.swift
@@ -0,0 +1,18 @@
+//
+//  Stocks.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Leandro Wauters on 12/8/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct Stocks: Codable {
+    let date: String
+    let open: Double
+    let high: Double
+    let low: Double
+    let close: Double
+    let label: String
+}
