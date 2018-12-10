782ebf3fd895d88df0edf7fcfcdbb49f50122315
Donkemezuo <raymonddonkemezuo.org>
2018-12-10 18:08:56 -0500

    Almost done with the first part

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/ContactViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/ContactViewController.swift
@@ -11,16 +11,34 @@ import UIKit
 class ContactViewController: UIViewController {
     
  var person = [resultsWrapper]()
+    var sortedPeople = [resultsWrapper]() {
+        
+        didSet {
+            peopleTableView.reloadData()
+        }
+    }
+    
+    @IBOutlet weak var mySearchBar: UISearchBar!
     
     @IBOutlet weak var peopleTableView: UITableView!
     override func viewDidLoad() {
+        
     super.viewDidLoad()
+         self.title = "Contacts"
     loadData()
         peopleTableView.dataSource = self
         peopleTableView.delegate = self
-        title = "Contacts"
-    // Do any additional setup after loading the view, typically from a nib.
+        mySearchBar.delegate = self
+        
   }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let myContactIndexPath = peopleTableView.indexPathForSelectedRow, let contactDetails = segue.destination as? PeopleDetailViewController else {return}
+        let individual = sortedPeople[myContactIndexPath.row]
+        contactDetails.presentPerson = individual
+
+        }
+    
     func loadData(){
         if let pathway = Bundle.main.path(forResource: "userinfo", ofType: "json"){
             let myUrl = URL.init(fileURLWithPath: pathway)
@@ -29,7 +47,7 @@ class ContactViewController: UIViewController {
                 do{
                     let dicts = try JSONDecoder().decode(UserInformation.self, from: data)
                  person = dicts.results
-             
+                    sortedPeople = person.sorted(by: {$0.name.last < $1.name.last})
                 } catch {
                     print(error)
                 }
@@ -43,16 +61,26 @@ class ContactViewController: UIViewController {
 extension ContactViewController: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = peopleTableView.dequeueReusableCell(withIdentifier: "peopleCell", for: indexPath)
-        let personToSet = person[indexPath.row]
+        let personToSet = sortedPeople[indexPath.row]
       cell.textLabel?.text = "\(personToSet.name.last.capitalized)  \(personToSet.name.first.capitalized)"
         cell.detailTextLabel?.text = personToSet.location.city
 return cell
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return person.count
+       return sortedPeople.count
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
+    }
+
+    }
+extension ContactViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        guard let searchWord = searchBar.text else {return}
+        sortedPeople = sortedPeople.filter{$0.name.last.contains(searchWord.lowercased())}
+        peopleTableView.reloadData()
+        
     }
 }
 
--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/PeopleDetailViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/PeopleDetailViewController.swift
@@ -9,24 +9,30 @@
 import UIKit
 
 class PeopleDetailViewController: UIViewController {
+    var presentPerson:resultsWrapper!
     
-   
-
+    @IBOutlet weak var personPhoto: UIImageView!
+    
+    @IBOutlet weak var personName: UILabel!
+    
+    @IBOutlet weak var personEmail: UILabel!
+    
+    @IBOutlet weak var personCity: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        updatePersonInfo()
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+    private func updatePersonInfo (){
+      personPhoto.image = UIImage.init(named: "profileImage")
+        personName.text = "\(presentPerson.name.last.capitalized) \(presentPerson.name.first.capitalized)"
+        personEmail.text = presentPerson.email
+        personCity.text = presentPerson.location.city.capitalized
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
     }
-    */
 
 }
