c99a902e5e1edacb73ec5da1b0944fb6d63f7fdd
janezhu1618 <janezhu@pursuit.org>
2018-12-11 14:11:55 -0500

    7th commit - refactored stocks VC to have section title display correct month, year, and average price

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Model/Stocks.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Model/Stocks.swift
@@ -14,12 +14,61 @@ struct Stocks: Codable {
     let close: Double
 }
 
-struct calculate {
-    static func monthlyAverage(from arr: [Stocks]) -> Double {
+struct StocksHelperPropertiesAndMethods {
+    static var monthDictionary: [String: String] = ["01":"January", "02":"February", "03":"March", "04":"April", "05":"May", "06":"June", "07":"July", "08":"August", "09":"September", "10":"October", "11":"November", "12":"December"]
+    
+    static func formatMonthCorrectly(_ month: Int) -> String {
+        switch month {
+        case 1:
+            return "01"
+        case 2:
+            return "02"
+        case 3:
+            return "03"
+        case 4:
+            return "04"
+        case 5:
+            return "05"
+        case 6:
+            return "06"
+        case 7:
+            return "07"
+        case 8:
+            return "08"
+        case 9:
+            return "09"
+        case 10:
+            return "10"
+        case 11:
+            return "11"
+        case 12:
+            return "12"
+        default:
+            return "invalid month"
+        }
+    }
+    
+    static func calculateMonthlyAverage(from arr: [Stocks]) -> Double {
         var returnDouble: Double = 0
         for element in arr {
             returnDouble += element.open
         }
         return returnDouble/Double(arr.count)
+    }
+    
+    static func monthLookingFor(is arr: [String], from month: String) -> Bool {
+        if arr[1] == month {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    static func yearLookingFor(is arr: [String], from year: String) -> Bool {
+        if arr[0] == year {
+            return true
+        } else {
+            return false
+        }
     }
 }
--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/New Group/StocksViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/New Group/StocksViewController.swift
@@ -13,16 +13,7 @@ class StocksViewController: UIViewController {
     @IBOutlet weak var stocksTableView: UITableView!
     
     var stocks: [Stocks] = []
-//    {
-//        didSet {
-//            stocksTableView.reloadData()
-//        }
-//    }
-    var stocksByMonthAndYear: [[Stocks]] = [] {
-        didSet {
-            stocksTableView.reloadData()
-        }
-    }
+    var stocksByMonthAndYear: [[Stocks]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,10 +22,10 @@ class StocksViewController: UIViewController {
         stocksTableView.delegate = self
         for year in 2016...2018 {
             for month in 1...12 {
-                let correctMonth = formatMonthCorrectly(month)
+                let formattedMonth = StocksHelperPropertiesAndMethods.formatMonthCorrectly(month)
                 let results = stocks.filter{
-                    let arr = $0.date.components(separatedBy: "-")
-                    if monthLookingFor(is: arr, from: correctMonth) && yearLookingFor(is: arr, from: year.description) {
+                    let dateAsArr = $0.date.components(separatedBy: "-")
+                    if StocksHelperPropertiesAndMethods.monthLookingFor(is: dateAsArr, from: formattedMonth) && StocksHelperPropertiesAndMethods.yearLookingFor(is: dateAsArr, from: year.description) {
                         return true
                     } else {
                         return false
@@ -45,56 +36,9 @@ class StocksViewController: UIViewController {
                 }
             }
         }
-        //print(stocksByMonthAndYear[0].filter{$0.open > 0})
-        //print(stocksByMonthAndYear)
     }
     
-    private func formatMonthCorrectly(_ month: Int) -> String {
-        switch month {
-        case 1:
-            return "01"
-        case 2:
-            return "02"
-        case 3:
-            return "03"
-        case 4:
-            return "04"
-        case 5:
-            return "05"
-        case 6:
-            return "06"
-        case 7:
-            return "07"
-        case 8:
-            return "08"
-        case 9:
-            return "09"
-        case 10:
-            return "10"
-        case 11:
-            return "11"
-        case 12:
-            return "12"
-        default:
-            return "invalid month"
-        }
-    }
     
-    private func monthLookingFor(is arr: [String], from month: String) -> Bool {
-        if arr[1] == month {
-            return true
-        } else {
-            return false
-        }
-    }
-        
-    private func yearLookingFor(is arr: [String], from year: String) -> Bool {
-        if arr[0] == year {
-            return true
-        } else {
-            return false
-        }
-    }
     
     func loadData() {
         if let path = Bundle.main.path(forResource: "applstockinfo", ofType: "json") {
@@ -124,65 +68,16 @@ extension StocksViewController: UITableViewDataSource {
         let cell = stocksTableView.dequeueReusableCell(withIdentifier: "stocksCell", for: indexPath)
         let stockToSet = stocksByMonthAndYear[indexPath.section][indexPath.row]
         cell.textLabel?.text = stockToSet.date
-        cell.detailTextLabel?.text = stockToSet.open.description
+        cell.detailTextLabel?.text = "$" + String(format: "%.2f", stockToSet.open)
         return cell
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section {
-            case 0:
-                return "December 2016 ~ Average: $\(String(format: "%.2f", calculate.monthlyAverage(from: stocksByMonthAndYear[0])))"
-            case 1:
-            return "January 2017: Average"
-            case 2:
-            return "February 2017: Average"
-            case 3:
-            return "March 2017: Average"
-            case 4:
-            return "April 2017: Average"
-            case 5:
-            return "May 2017: Average"
-            case 6:
-            return "June 2017: Average"
-            case 7:
-            return "July 2017: Average"
-            case 8:
-            return "August 2017: Average"
-            case 9:
-            return "September 2017: Average"
-            case 10:
-            return "October 2017: Average"
-            case 11:
-            return "November 2017: Average"
-            case 12:
-            return "December 2017: Average"
-            case 13:
-            return "January 2018: Average"
-            case 14:
-            return "February 2018: Average"
-            case 15:
-            return "March 2018: Average"
-            case 16:
-            return "April 2018: Average"
-            case 17:
-            return "May 2018: Average"
-            case 18:
-            return "June 2018: Average"
-            case 19:
-            return "July 2018: Average"
-            case 20:
-            return "August 2018: Average"
-            case 21:
-            return "September 2018: Average"
-            case 22:
-            return "October 2018: Average"
-            case 23:
-            return "November 2018: Average"
-            case 24:
-            return "December 2018: Average"
-            default:
-            return "date not found"
-        }
+        let dateAsArr = stocksByMonthAndYear[section].first!.date.components(separatedBy: "-")
+        let month = StocksHelperPropertiesAndMethods.monthDictionary[dateAsArr[1]]
+        let year = dateAsArr[0]
+        let average = String(format: "%.2f", StocksHelperPropertiesAndMethods.calculateMonthlyAverage(from: stocksByMonthAndYear[section]))
+        return month! + " " + year + " ~ Average: $" + average
     }
 }
 
