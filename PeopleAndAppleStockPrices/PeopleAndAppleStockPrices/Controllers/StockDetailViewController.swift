af71c2cc8789399ed5a41e082cc1ae85622dac40
kwaring3 <kevinwaring@pursuit.org>
2018-12-12 16:38:01 -0500

    3rd commit

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controllers/StockDetailViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controllers/StockDetailViewController.swift
@@ -13,11 +13,25 @@ class StockDetailViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var openLabel: UILabel!
     @IBOutlet weak var closeLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
+    private func setUp(){
+        guard let appl = appl else {fatalError("nil")}
+        dateLabel.text = appl.date
+        openLabel.text = "Open $\(String(format: "%.2f", appl.open))"
+        closeLabel.text = "Close $\(String(format: "%.2f", appl.close))"
+        if appl.open < appl.close {
+            imageView.image = UIImage(named: "thumbs up")
+            view.backgroundColor = .green
+        } else if appl.open > appl.close {
+            imageView.image = UIImage(named: "thumbsDown")
+            view.backgroundColor = .red 
+        }
+        }
     
 
     
