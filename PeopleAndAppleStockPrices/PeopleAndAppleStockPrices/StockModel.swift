b90d93670e41cf784b23dfe8ac1121ae425168ec
Michelle <Michelle.cueva@gmail.com>
2019-08-30 17:46:30 -0400

    Adds test to see if appleJSONData loads correctly

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/StockModel.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/StockModel.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+struct Stock: Codable {
+    let date: String
+    let openPrice: String
+    let closePrice: String
+    
+    
+    private enum CodingKeys: String, CodingKey {
+        case date
+        case openPrice = "uOpen"
+        case closePrice = "uClose"
+    }
+    
+    static func getStock(from data: Data) -> [Stock]? {
+        do {
+            let newStock = try JSONDecoder().decode([Stock].self, from: data)
+            return newStock
+        } catch {
+            return nil
+        }
+    }
+}
