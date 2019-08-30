a92d3e36b9e608c4ca3b640e2145e5bcc6ec47ac
levi davis <levidavis@pursuit.org>
2019-08-30 16:49:27 -0400

    Sets up PeopleViewController and cells

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/View Controllers/PeopleViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/View Controllers/PeopleViewController.swift
@@ -10,22 +10,50 @@ import UIKit
 
 class PeopleViewController: UIViewController {
 
+    var contacts = [Contact]() {
+        didSet {
+            contactsTableView.reloadData()
+        }
+    }
+    
     @IBOutlet weak var contactsTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        contactsTableView.delegate = self
+        contactsTableView.dataSource = self
+        loadData()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func loadData() {
+        guard let pathToJSONFile = Bundle.main.path(forResource: "userinfo", ofType: "json") else {fatalError("Could not find path")}
+        let url = URL(fileURLWithPath: pathToJSONFile)
+        do {
+            let data = try Data(contentsOf: url)
+            let contactsFromJSON = Results.getContacts(from: data)
+            contacts = contactsFromJSON
+        } catch let loadDataError {
+            fatalError("Error: \(loadDataError)")
+        }
     }
-    */
 
+}
+
+extension PeopleViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = contactsTableView.dequeueReusableCell(withIdentifier: "contactsCell", for: indexPath)
+        let oneContact = contacts[indexPath.row]
+        cell.textLabel?.text = "\(oneContact.name.first) \(oneContact.name.last)"
+        cell.detailTextLabel?.text = oneContact.location.city
+        return cell
+    }
+    
+    
 }
