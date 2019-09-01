4f826d057f8436e3fd8c7ee0515764749dcc01a3
Michelle <Michelle.cueva@gmail.com>
2019-09-01 17:14:42 -0400

    Does auto layout for User Detail ViewController

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Main View Controllers/StocksViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Main View Controllers/StocksViewController.swift
@@ -9,22 +9,29 @@
 import UIKit
 
 class StocksViewController: UIViewController {
+    
 
+    @IBOutlet weak var stocksTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+//extension StocksViewController : UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//
+//}
+//
+//extension StocksViewController: UITableViewDelegate {
+//
+//}
