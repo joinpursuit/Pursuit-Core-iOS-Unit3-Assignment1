fb92d85d46da251cffeb80c4d5ea81617286c318
Jason Ruan <jasonruan@pursuit.org>
2019-09-04 17:36:17 -0400

    Defines Contacts model struct and relevant wrappers

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Models/Contacts.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Models/Contacts.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+struct Contacts: Codable {
+    let people: [Person]?
+    
+    private enum CodingKeys: String, CodingKey {
+        case people = "results"
+    }
+}
+
+struct Person: Codable {
+    let name: NameWrapper
+    let location: LocationWrapper
+    let picture: PictureWrapper
+    let email: String?
+}
+
+struct NameWrapper: Codable {
+    let title: String?
+    let first: String?
+    let last: String?
+}
+
+struct LocationWrapper: Codable {
+    let street: String?
+    let city: String?
+    let state: String?
+}
+
+struct PictureWrapper: Codable {
+    let large: URL?
+}
