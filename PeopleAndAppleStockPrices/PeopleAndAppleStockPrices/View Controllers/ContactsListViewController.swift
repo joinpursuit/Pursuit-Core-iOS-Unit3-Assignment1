606787dc6ce36e573ce9c2d568570855f51b8673
Jason Ruan <jasonruan@pursuit.org>
2019-09-04 17:21:16 -0400

    Adds view controller files and models, organizes them into their own folders

---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/View Controllers/ContactsListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactsListViewController.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Jason Ruan on 9/4/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ContactsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/View Controllers/StockDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StockDetailsViewController.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Jason Ruan on 9/4/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class StockDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
