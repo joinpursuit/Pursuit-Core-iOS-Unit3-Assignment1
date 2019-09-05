8f276cb421b4da5855cbed208b38cab4df77335f
Jason Ruan <jasonruan@pursuit.org>
2019-09-05 10:40:57 -0400

    Adds prepare segue function in the contactsListVC

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/View Controllers/ContactsListViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/View Controllers/ContactsListViewController.swift
@@ -68,14 +68,17 @@ class ContactsListViewController: UIViewController, UITableViewDataSource {
         }
     }
 
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let segueIdentifier = segue.identifier else { return }
+        switch segueIdentifier {
+        case "contactInfo":
+            guard let contactInfoVC = segue.destination as? ContactDetailsViewController else { return }
+            guard let pathForSelectedContact = contactsListTableView.indexPathForSelectedRow else { return }
+            contactInfoVC.person = filteredContactsList[pathForSelectedContact.row]
+        default:
+            return
+        }
     }
-    */
 
 }
