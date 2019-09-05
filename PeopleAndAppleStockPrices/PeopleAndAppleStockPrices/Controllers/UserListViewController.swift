177f1392be1d61a04be07bb84868e1022035f824
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-04 20:25:26 -0400

    Adds delegate and datasource functions to UserListVC

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controllers/UserListViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controllers/UserListViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class UserListViewController: UIViewController {
-
+    
+    @IBOutlet weak var userListTableView: UITableView!
+    
+    var allUsers = [User]() {
+        didSet {
+            userListTableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTableView()
     }
-
+    
+    private func configureTableView() {
+        userListTableView.dataSource = self
+        userListTableView.delegate = self
+    }
 
 }
 
+extension UserListViewController: UITableViewDelegate {}
+
+extension UserListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
