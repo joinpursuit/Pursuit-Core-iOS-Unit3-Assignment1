89cf8ac5d4faede6f42f679d8f9782c32af5888b
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-05 17:46:16 -0400

    adds addSectionNames func in StockListVC and populates stockListTableView delegate and datasource methods

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controllers/StockListViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controllers/StockListViewController.swift
@@ -18,35 +18,64 @@ class StockListViewController: UIViewController {
         }
     }
     
+    var headerNames = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        loadStockData()
+        addSectionNames()
     }
     
     private func configureTableView() {
         stockListTableView.dataSource = self
         stockListTableView.delegate = self
     }
-
+    
+    private func loadStockData() {
+        let stockData = DataAPIClient.getStockDataFromJSON()
+        // refactor later to not force unwrap?
+        allStocks = Stock.getAllStocks(from: stockData)!
+    }
+    
 }
 
 extension StockListViewController: UITableViewDelegate {
+    private func addSectionNames() {
+        for stock in allStocks where !headerNames.contains(stock.dateForHeader) {
+            headerNames.append(stock.dateForHeader)
+        }
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        print(headerNames.count)
+        return headerNames.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "?"
+        print(headerNames[section])
+        return headerNames[section]
     }
 }
 
 extension StockListViewController: UITableViewDataSource {
+//    private func filterStockBySection(section: Int) -> [Stock] {
+//        let stocksForSection = allStocks.filter { $0.dateForHeader == headerName[section] }
+//        return stocksForSection
+//    }
+//
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allStocks.count
+        let stocksForSection = allStocks.filter { $0.dateForHeader == headerNames[section] }
+        print(stocksForSection)
+        return stocksForSection.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let stockCell = stockListTableView.dequeueReusableCell(withIdentifier: "stockCell", for: indexPath)
+        let currentStock = allStocks[indexPath.row]
+        stockCell.textLabel?.text = currentStock.date
+        
+        return stockCell
     }
     
     
--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Models/Stock.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Models/Stock.swift
@@ -13,6 +13,7 @@ struct Stock: Codable {
     let open: Double
     let close: Double
     
+    
     var dateForHeader: String {
         var dateArr = date.components(separatedBy: "-")
         let year = dateArr[0]
@@ -28,5 +29,4 @@ struct Stock: Codable {
             return nil
         }
     }
-    
 }
