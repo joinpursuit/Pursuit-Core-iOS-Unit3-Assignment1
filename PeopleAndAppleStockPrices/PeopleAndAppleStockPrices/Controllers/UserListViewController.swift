31076acc145b39a94bf1b7f8fff2e4c20a3b5fa1
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-05 14:04:46 -0400

    adds search bar method and delegate to UserListVC and filters tableview by first name

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controllers/UserListViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Controllers/UserListViewController.swift
@@ -10,23 +10,44 @@ import UIKit
 
 class UserListViewController: UIViewController {
     
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var userListTableView: UITableView!
     
     var allUsers = [User]() {
-        didSet {
-            userListTableView.reloadData()
+        didSet { self.userListTableView.reloadData() }
+    }
+    
+    var searchString: String? = nil {
+        didSet { self.userListTableView.reloadData() }
+    }
+    
+    var userSearchResults: [User] {
+        get {
+            guard let searchString = searchString else { return allUsers }
+            guard searchString != "" else { return allUsers }
+        
+            let filterFirstNameArr = allUsers.filter{$0.name.firstName.lowercased().contains(searchString.lowercased())}
+//            let filterLastNameArr = filterFirstNameArr.filter{$0.name.lastName.lowercased().contains(searchString.lowercased())}
+            return filterFirstNameArr
         }
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        configureSearchBar()
         loadUserData()
     }
     
     private func configureTableView() {
         userListTableView.dataSource = self
         userListTableView.delegate = self
+    }
+    
+    private func configureSearchBar() {
+        searchBar.delegate = self
     }
     
     private func loadUserData() {
@@ -50,11 +71,11 @@ extension UserListViewController: UITableViewDelegate {}
 
 extension UserListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allUsers.count
+        return userSearchResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let currentUser = allUsers[indexPath.row]
+        let currentUser = userSearchResults[indexPath.row]
         let userCell = userListTableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath)
         
         //refactor to capitalize name in models
@@ -63,6 +84,10 @@ extension UserListViewController: UITableViewDataSource {
         
         return userCell
     }
-    
-    
+}
+
+extension UserListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchString = searchBar.text
+    }
 }
