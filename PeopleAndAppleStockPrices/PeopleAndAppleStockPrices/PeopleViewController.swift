d5e9afa4eb6619e3d1231c9c678fde62e094d4d3
Krystal Campbell <krystalcampbell@Krystals-MBP.fios-router.home>
2019-09-05 22:12:58 -0400

    search bar up and running

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/PeopleViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/PeopleViewController.swift
@@ -10,80 +10,73 @@ import UIKit
 
 class PeopleViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
     
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet var userSearchBar: UISearchBar!
     
     @IBOutlet weak var peopleTableView: UITableView!
     
     var peopleArr = [resultsInfo]() {
         didSet{
             peopleTableView.reloadData()
-            
         }
     }
     
-    var filteredPeople: [resultsInfo] {
-        get {
-            guard searchedText != "" else { return peopleArr }
-            print("searched text")
-            return peopleArr.filter{$0.name.first.contains(searchedText)}
-        }
-    }
-    
-    var searchedText: String = "" {
+    var userSearchedText: String? {
         didSet {
-            peopleTableView.reloadData()
+            self.peopleTableView.reloadData()
         }
     }
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-       // searchedText = searchText
-        self.searchedText = searchText
- 
-
-        
-        //goal is to change characters in search bar as user inputs letters.
+    var filteredPeople: [resultsInfo]  {
+        guard let searchedText = userSearchedText else {
+            return peopleArr
+        }
+    return peopleArr.filter({$0.name.first.lowercased().contains(searchedText.lowercased())})
+     
     }
     
-    private func loadData() {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.userSearchedText = searchBar.text
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.userSearchedText = searchText
+        
+    }
+        private func loadData() {
         PeopleAPIManager.shared.getPeople {(result) in
-            DispatchQueue.main.async {
-                switch result {
-                case .failure(let error):
-                    print(error)
-                case .success(let peopleArrFromOnline):
-                    self.peopleArr = peopleArrFromOnline
-                }
+        DispatchQueue.main.async {
+        switch result {
+        case .failure(let error):
+        print(error)
+        case .success(let peopleArrFromOnline):
+        self.peopleArr = peopleArrFromOnline
             }
         }
-        
-        
     }
+}
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       // return peopleArr.count
         return filteredPeople.count
         
-    }
-    
+        }
+        
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = peopleTableView.dequeueReusableCell(withIdentifier: "PeopleCell", for: indexPath)
-        //let person = peopleArr[indexPath.row]
         let person = filteredPeople[indexPath.row]
         cell.textLabel?.text = "\(person.name.first.capitalized) \(person.name.last.capitalized)"
         cell.detailTextLabel?.text = person.location.state
         return cell
-    }
-    
-    override func viewDidLoad() {
+        }
+        
+        override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
         peopleTableView.dataSource = self
-        searchBar.delegate = self
-        //    peopleTableView.delegate = self
+        userSearchBar.delegate = self
+    //    peopleTableView.delegate = self
+        
+        }
         
     }
-    
-}
-
 
 //
 //    ImageHelper.shared.getImage(urlStr: thisBeer.imageURL) { (result) in
@@ -106,3 +99,4 @@ class PeopleViewController: UIViewController, UITableViewDataSource, UISearchBar
 //
 //}
 //
+
