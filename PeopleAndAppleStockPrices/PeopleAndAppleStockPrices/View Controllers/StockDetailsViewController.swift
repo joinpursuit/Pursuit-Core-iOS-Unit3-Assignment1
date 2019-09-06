d2ad4d54c7983e1c815d9d056f07da1241b0bbdd
Jason Ruan <jasonruan@pursuit.org>
2019-09-05 22:14:25 -0400

    Populates stockDetailsVC and creates functions to setup the views

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/View Controllers/StockDetailsViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/View Controllers/StockDetailsViewController.swift
@@ -9,22 +9,52 @@
 import UIKit
 
 class StockDetailsViewController: UIViewController {
-
+    
+    //MARK: -- Properties
+    var stockForDate: StockPrice!
+    
+    //MARK: -- IBOutlets
+    @IBOutlet weak var stockImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var openingLabel: UILabel!
+    @IBOutlet weak var closingLabel: UILabel!
+    @IBOutlet weak var opPriceLabel: UILabel!
+    @IBOutlet weak var edPriceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpViews() {
+        setUpLabels()
+        setVisuals()
     }
-    */
-
+    
+    private func setUpLabels() {
+        dateLabel.text = "\(stockForDate.date!)"
+        openingLabel.text = "$\(stockForDate.open!)"
+        closingLabel.text = "$\(stockForDate.close!)"
+    }
+    
+    private func setVisuals() {
+        if stockForDate.changePercent! > 0.0 {
+            stockImageView.image = UIImage(named: "thumbsUp")
+            self.view.backgroundColor = UIColor.green
+            changeTextColor(color: UIColor.black)
+        } else {
+            stockImageView.image = UIImage(named: "thumbsDown")
+            self.view.backgroundColor = UIColor.red
+            changeTextColor(color: UIColor.white)
+        }
+    }
+    
+    private func changeTextColor(color: UIColor) {
+        dateLabel.textColor = color
+        opPriceLabel.textColor = color
+        edPriceLabel.textColor = color
+        openingLabel.textColor = color
+        closingLabel.textColor = color
+    }
+    
 }
