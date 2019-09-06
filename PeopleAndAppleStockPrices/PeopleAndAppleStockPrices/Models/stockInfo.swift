e760d76d98bd901672c1c14f4d2766410fd435e4
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-09-06 16:34:55 -0400

    creates stockInfo model

---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Models/stockInfo.swift
@@ -0,0 +1,30 @@
+//
+//  stockInfo.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by Radharani Ribas-Valongo on 9/6/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+struct stockInfo: Codable {
+    let date: String
+    let stockPrice: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case date
+        case stockPrice = "high"
+    }
+    
+    static func getStocks(from data: Data) throws -> [stockInfo] {
+        do {
+            let stock = try JSONDecoder().decode([stockInfo].self, from: data)
+            return stock
+        } catch {
+            throw ErrorHandling.decodingError
+        }
+        
+    }
+}
+
