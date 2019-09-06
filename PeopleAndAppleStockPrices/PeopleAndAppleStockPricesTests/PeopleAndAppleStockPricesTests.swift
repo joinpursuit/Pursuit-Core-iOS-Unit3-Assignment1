9f61da17bcd73c54f1266094e167d6c37292a01f
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-09-06 16:23:47 -0400

    unit tests added

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPricesTests/PeopleAndAppleStockPricesTests.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPricesTests/PeopleAndAppleStockPricesTests.swift
@@ -10,25 +10,40 @@ import XCTest
 
 class PeopleAndAppleStockPricesTests: XCTestCase {
     
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    private func getStockDataFromJSON() -> Data {
+        guard let pathToData = Bundle.main.path(forResource: "applstockinfo", ofType:"json")
+            else {
+                fatalError("applstockinfo.json file not found")
+        }
+        let url = URL(fileURLWithPath: pathToData)
+        do {
+            let data = try Data(contentsOf: url)
+            return data
+        }catch let jsonError {
+            fatalError("could not find file: \(jsonError)")
+        }
+        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testStockDataLoad() {
+        let data = getStockDataFromJSON()
+        do {
+        let stocks = try Stocks.getStocksData(data: data)
+        XCTAssertTrue(stocks.count > 0, "no data mon")
+        }catch{
+            print(error)
+            
         }
     }
-
 }
+
+
+
