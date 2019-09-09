a465ac6b8f109504de582111f38de25bd4d87cf5
davidlawrencer <davidlrifkin+github@gmail.com>
2019-09-09 10:52:36 -0400

    Adds json decoder for Users file

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/User.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/User.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+struct UserWrapper: Codable {
+    let results: [User]
+}
+
 struct User: Codable {
     let name: Name
     let location: Location
@@ -32,6 +36,19 @@ struct User: Codable {
         private let city: String
         var address: String {
             return "\(street), \(city)"
+        }
+    }
+    
+    static func getUsers() -> [User] {
+        guard let fileName = Bundle.main.path(forResource: "userinfo", ofType: "json") else {fatalError()}
+        guard let fileURL = URL(string: fileName) else {fatalError()}
+        
+        do {
+            let data = try Data(contentsOf: fileURL)
+            let userWrapper = try JSONDecoder().decode(UserWrapper.self, from: data)
+            return userWrapper.results
+        } catch {
+            fatalError()
         }
     }
 }
