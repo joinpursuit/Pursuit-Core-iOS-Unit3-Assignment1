22210bc630eedb8e12c68d6e16ba3e459dc75f39
davidlawrencer <davidlrifkin+github@gmail.com>
2019-09-09 14:37:08 -0400

    Sets up computed properties on stock

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Stock.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/Stock.swift
@@ -9,9 +9,18 @@
 import Foundation
 
 struct Stock: Codable {
-    let date: String
+    private let date: String
     let opening: Double
     let closing: Double
+    var day: String {
+        return date.components(separatedBy: "-")[2]
+    }
+    var month: String {
+        return date.components(separatedBy: "-")[1]
+    }
+    var year: String {
+        return date.components(separatedBy: "-")[0]
+    }
     
     private enum CodingKeys: String, CodingKey {
         case date
