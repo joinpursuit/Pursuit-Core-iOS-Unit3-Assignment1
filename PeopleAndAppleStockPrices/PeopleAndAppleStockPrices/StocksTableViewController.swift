e613c4485d53d08039733b0a4f797126b5643349
davidlawrencer <davidlrifkin+github@gmail.com>
2019-09-09 10:02:54 -0400

    Adds view controller files and tableviews

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/StocksTableViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/StocksTableViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StocksTableViewController: UIViewController {
 
-  override func viewDidLoad() {
+    @IBOutlet weak var stocksTableView: UITableView!
+    override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
   }
-
-
 }
 
---
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/UsersTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UsersTableViewController.swift
+//  PeopleAndAppleStockPrices
+//
+//  Created by David Rifkin on 9/9/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewController: UIViewController {
+
+    @IBOutlet weak var usersSearchBar: UISearchBar!
+    @IBOutlet weak var userTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
